4b0b52e220dfb9ac1357862c1af2183a3c85e744
rloganayaki <loganayaki993@gmail.com>
2016-09-17 14:32:43 +0530

    finish

---
+++ README.C
@@ -0,0 +1,30 @@
+# km
+#include<stdio.h>
+int main(){
+char a[50]b[50];
+int c,d,m1[100]=-1,m2[100]=0;
+scanf("%s",&a);
+c=strlen(a);
+scanf("%s",b);
+d=strlen(b);
+if(c!=d)
+{
+return 0;
+}
+else
+{
+for(i=0;i<c;i++)
+{
+if(m1([ai])==-1)
+{
+if(m2(a[i])==1)
+return 0;
+m1(a[i])=b[i];
+m2(b[i])=b[i];
+elseif(m1(a[i]!=b[i])
+return 0;
+}
+}
+return 1;
+}
+}
